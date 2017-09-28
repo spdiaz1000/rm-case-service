469299f078c6cb292643c4465f5f87e187455d85
Philippe Brossier <brossierp@gmail.com>
2017-09-28 10:52:07 +0100

    CTPA-1511 sorting out the liquibase change

--- src/main/resources/database/changes/release-10.47.0/removal_of_report_from_displayname.sql
+++ src/main/resources/database/changes/release-10.47.0/removal_of_report_from_displayname.sql
@@ -7,23 +7,3 @@ SET displayname = 'Case Events' WHERE reporttypepk = 'CASE_EVENTS';
 
 UPDATE reporttype
 SET displayname = 'Response Chasing' WHERE reporttypepk = 'RESPONSE_CHASING';
-
-
-CREATE SEQUENCE casenotificationseq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    MAXVALUE 999999999999
-    CACHE 1;
-
-
-CREATE TABLE casenotification (
-    casenotificationPK bigint NOT NULL,
-    caseid uuid NOT NULL,
-    actionplanid uuid NOT NULL,
-    notificationtype character varying(20) NOT NULL
-);
-
-
-ALTER TABLE ONLY casenotification
-    ADD CONSTRAINT casenotification_pkey PRIMARY KEY (casenotificationPK);
---
+++ src/main/resources/database/changes/release-10.48.0/table_case_notification.sql
@@ -0,0 +1,21 @@
+SET schema 'casesvc';
+
+
+CREATE SEQUENCE casenotificationseq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 999999999999
+    CACHE 1;
+
+
+CREATE TABLE casenotification (
+    casenotificationPK bigint NOT NULL,
+    caseid uuid NOT NULL,
+    actionplanid uuid NOT NULL,
+    notificationtype character varying(20) NOT NULL
+);
+
+
+ALTER TABLE ONLY casenotification
+    ADD CONSTRAINT casenotification_pkey PRIMARY KEY (casenotificationPK);
