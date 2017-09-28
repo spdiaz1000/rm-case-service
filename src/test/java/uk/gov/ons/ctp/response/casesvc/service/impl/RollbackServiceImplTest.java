3b761ea8b18422ef5f5209d500c5b070716db558
Philippe Brossier <brossierp@gmail.com>
2017-09-28 13:46:08 +0100

    CTPA-1511 unti test for rollbackservice for method case distributor process case

--- src/test/java/uk/gov/ons/ctp/response/casesvc/service/impl/RollbackServiceImplTest.java
+++ src/test/java/uk/gov/ons/ctp/response/casesvc/service/impl/RollbackServiceImplTest.java
@@ -9,20 +9,24 @@ import uk.gov.ons.ctp.response.casesvc.domain.model.Case;
 import uk.gov.ons.ctp.response.casesvc.domain.model.CaseNotification;
 import uk.gov.ons.ctp.response.casesvc.domain.repository.CaseNotificationRepository;
 import uk.gov.ons.ctp.response.casesvc.domain.repository.CaseRepository;
+import uk.gov.ons.ctp.response.casesvc.representation.CaseState;
 
 import java.util.UUID;
 
 import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+import static uk.gov.ons.ctp.response.casesvc.scheduled.distribution.CaseDistributor.METHOD_CASE_DISTRIBUTOR_PROCESS_CASE;
 import static uk.gov.ons.ctp.response.casesvc.service.impl.CaseServiceImpl.COMMA;
 import static uk.gov.ons.ctp.response.casesvc.service.impl.RollbackServiceImpl.UNEXPECTED_SITUATION_ERRRO_MSG;
 import static uk.gov.ons.ctp.response.casesvc.service.impl.SupportServiceImpl.METHOD_SUPPORT_SERVICE_REPLAY;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RollbackServiceImplTest {
+
+  private static final String CASE_ID_1 ="551308fb-2d5a-4477-92c3-649d915834c1";
 
   @Mock
   private CaseRepository caseRepo;
@@ -43,7 +47,7 @@ public class RollbackServiceImplTest {
   }
 
   @Test
-  public void testCaseNotificationPublishMethodReplayMsgNotReturned() {
+  public void testCaseNotificationPublishMethodSupportServiceReplayMsgNotReturned() {
     StringBuffer correlationDataId = new StringBuffer(METHOD_SUPPORT_SERVICE_REPLAY);
     correlationDataId.append(COMMA);
     correlationDataId.append("1");
@@ -55,7 +59,7 @@ public class RollbackServiceImplTest {
   }
 
   @Test
-  public void testCaseNotificationPublishMethodReplayMsgReturned() {
+  public void testCaseNotificationPublishMethodSupportServiceReplayMsgReturned() {
     StringBuffer correlationDataId = new StringBuffer(METHOD_SUPPORT_SERVICE_REPLAY);
     correlationDataId.append(COMMA);
     correlationDataId.append("1");
@@ -68,6 +72,40 @@ public class RollbackServiceImplTest {
 
     verify(caseRepo, never()).findById(any(UUID.class));
     verify(caseRepo, never()).saveAndFlush(any(Case.class));
+    verify(caseNotificationRepository, never()).saveAndFlush(any(CaseNotification.class));
+  }
+
+  @Test
+  public void testCaseNotificationPublishMethodCaseDistributorProcessCaseNoCaseFound() {
+    StringBuffer correlationDataId = new StringBuffer(METHOD_CASE_DISTRIBUTOR_PROCESS_CASE);
+    correlationDataId.append(COMMA);
+    correlationDataId.append(CASE_ID_1);
+    correlationDataId.append(COMMA);
+    correlationDataId.append(CaseState.ACTIONABLE.name());
+    rollbackService.caseNotificationPublish(correlationDataId.toString(), false);
+
+    verify(caseRepo, times(1)).findById(eq(UUID.fromString(CASE_ID_1)));
+    verify(caseRepo, never()).saveAndFlush(any(Case.class));
+    verify(caseNotificationRepository, never()).saveAndFlush(any(CaseNotification.class));
+  }
+
+  @Test
+  public void testCaseNotificationPublishMethodCaseDistributorProcessCas() {
+    UUID caseId = UUID.fromString(CASE_ID_1);
+    Case caze = Case.builder().state(CaseState.ACTIONABLE).iac("ABCD EFGH IJKL MNOP").build();
+    when(caseRepo.findById(caseId)).thenReturn(caze);
+
+    StringBuffer correlationDataId = new StringBuffer(METHOD_CASE_DISTRIBUTOR_PROCESS_CASE);
+    correlationDataId.append(COMMA);
+    correlationDataId.append(CASE_ID_1);
+    correlationDataId.append(COMMA);
+    correlationDataId.append(CaseState.ACTIONABLE.name());
+    rollbackService.caseNotificationPublish(correlationDataId.toString(), false);
+
+    verify(caseRepo, times(1)).findById(eq(caseId));
+    caze.setIac(null);
+    caze.setState(CaseState.ACTIONABLE);
+    verify(caseRepo, times(1)).saveAndFlush(eq(caze));
     verify(caseNotificationRepository, never()).saveAndFlush(any(CaseNotification.class));
   }
 }
