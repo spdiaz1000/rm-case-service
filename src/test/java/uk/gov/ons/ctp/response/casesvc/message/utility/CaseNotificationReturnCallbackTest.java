070081170255bdf5eb2338dab8f3688c62fcbeeb
Philippe Brossier <brossierp@gmail.com>
2017-09-28 14:42:32 +0100

    CTPA-1511 unit test for CaseNotificationReturnCallback

---
+++ src/test/java/uk/gov/ons/ctp/response/casesvc/message/utility/CaseNotificationReturnCallbackTest.java
@@ -0,0 +1,36 @@
+package uk.gov.ons.ctp.response.casesvc.message.utility;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import uk.gov.ons.ctp.response.casesvc.service.RollbackService;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static uk.gov.ons.ctp.response.casesvc.message.impl.CaseNotificationPublisherImpl.HEADER_USED_TO_ROLLBACK;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CaseNotificationReturnCallbackTest {
+
+  @Mock
+  private RollbackService rollbackService;
+
+  @InjectMocks
+  private CaseNotificationReturnCallback caseNotificationReturnCallback;
+
+  @Test
+  public void testReturnedMessage() {
+    MessageProperties messageProps = new MessageProperties();
+    String correlationIdString = "unitTest";
+    messageProps.setHeader(HEADER_USED_TO_ROLLBACK, correlationIdString);
+    Message message = new Message(null, messageProps);
+    caseNotificationReturnCallback.returnedMessage(message, 1, "replyText", "exchange", "routingKey");
+
+    Mockito.verify(rollbackService, times(1)).caseNotificationPublish(eq(correlationIdString), eq(true));
+  }
+}
