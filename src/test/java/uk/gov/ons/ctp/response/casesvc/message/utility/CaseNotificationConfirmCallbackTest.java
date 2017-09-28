71ed62a681c2ec168bcb31aca69522157b61bf97
Philippe Brossier <brossierp@gmail.com>
2017-09-28 14:27:13 +0100

    CTPA-1511 unti test for CaseNotificationConfirmCallback

---
+++ src/test/java/uk/gov/ons/ctp/response/casesvc/message/utility/CaseNotificationConfirmCallbackTest.java
@@ -0,0 +1,49 @@
+package uk.gov.ons.ctp.response.casesvc.message.utility;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.amqp.rabbit.support.CorrelationData;
+import uk.gov.ons.ctp.response.casesvc.service.RollbackService;
+import uk.gov.ons.ctp.response.casesvc.service.SupportService;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CaseNotificationConfirmCallbackTest {
+
+  @Mock
+  private RollbackService rollbackService;
+
+  @Mock
+  private SupportService supportService;
+
+  @InjectMocks
+  private CaseNotificationConfirmCallback caseNotificationConfirmCallback;
+
+  @Test
+  public void testConfirmAcktrue() {
+    CorrelationData correlationData = new CorrelationData();
+    String correlationDataId = "unitTest";
+    correlationData.setId(correlationDataId);
+    caseNotificationConfirmCallback.confirm(correlationData, true, null);
+
+    Mockito.verify(rollbackService, Mockito.never()).caseNotificationPublish(eq(correlationDataId), eq(false));
+    Mockito.verify(supportService, times(1)).removeFromDatabase(eq(correlationDataId));
+  }
+
+  @Test
+  public void testConfirmAckFalse() {
+    CorrelationData correlationData = new CorrelationData();
+    String correlationDataId = "unitTest";
+    correlationData.setId(correlationDataId);
+    caseNotificationConfirmCallback.confirm(correlationData, false, null);
+
+    Mockito.verify(rollbackService, times(1)).caseNotificationPublish(eq(correlationDataId), eq(false));
+    Mockito.verify(supportService, Mockito.never()).removeFromDatabase(eq(correlationDataId));
+  }
+}
