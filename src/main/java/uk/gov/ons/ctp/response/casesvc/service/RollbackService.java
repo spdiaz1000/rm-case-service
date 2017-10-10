7898826ece4a72016355367856c5b932595ebe18
Philippe Brossier <brossierp@gmail.com>
2017-10-10 12:03:24 +0100

    CTPA-1511 javadoc had been forgottten

--- src/main/java/uk/gov/ons/ctp/response/casesvc/service/RollbackService.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/service/RollbackService.java
@@ -1,16 +1,20 @@
 package uk.gov.ons.ctp.response.casesvc.service;
 
 /**
- * The service dedicated to rollbacks required when a published message fails to reach a RabbitMQ queue. This occurs
- * when an exchange or a queue is badly configured/deleted.
+ * The service dedicated to actions required when a published message fails to reach a RabbitMQ queue. This
+ * occurs when an exchange or a queue is badly configured/deleted.
  */
 public interface RollbackService {
 
   /**
-   * To rollback actions taken prior to the publish of CaseNotification using NotificationPublisher. This is used when
-   * a message does not reach the queue because the exchange is incorrectly set up, the queue has been deleted or?
+   * To rollback actions taken prior to the publish of CaseNotification using NotificationPublisher
+   * or
+   * To store failed messages to DB
    *
-   * @param correlationDataId a string made up of method name where NotificationPublisher was called,caseId,previousState
+   * This is used when a message does not reach the queue because the exchange is incorrectly set up, the queue has been
+   * deleted or?
+   *
+   * @param correlationDataId the data required to rollback or store to DB
    * @param msgReturned true if the message was returned (ie case where the queue was deleted)
    */
   void caseNotificationPublish(String correlationDataId, boolean msgReturned);
